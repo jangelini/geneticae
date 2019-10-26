e82982f5e05764be24445dbf5cf94c0ea16b0c25
jangelini <jangelini_93@hotmail.com>
2019-10-26 10:56:38 -0300

    Neew

--- R/impute.R
+++ R/impute.R
@@ -1,84 +1,106 @@
 #' Produces genotype plus genotype-by-environment model from a 2-way table of
 #' means
 #'
-#' @param Data a data frame or matrix that contains the genotypes in the rows and the
-#'   environments in the columns when there are no replications of the experiment. In case
-#'   of replications, the data frame must have the columns in the following order: genotypes,
-#'   environments, replications and the response variable.
-#' @param rep logical. If TRUE the genotype by environment means is calculated.
+#' @param Data a data frame
+#' @param genotype name of the column that contains the genotypes
+#' @param environment name of the column that contains the environments
+#' @param response name of the column that contains the response
+#' @param rep name of the column that contains the replications.If this argument
+#'   is NULL, there is no replications in the data.
 #' @param type imputation method. Either "EM-AMMI", "EM-SVD", "Gabriel","WGabriel"
+#' @inheritParams EM.AMMI
+#' @inheritParams bcv::impute.svd
+#' @inheritParams missMDA::imputePCA
+#' @inheritParams Gabriel.Calinski
+#' @inheritParams WGabriel
 #' @return matrix with imputed data
 #' @export
 #'
 #' @examples
 #' library(geneticae)
-#' library(reshape2)
+#' # Data without replication
 #' data(yan.winterwheat)
-#' dat1 <- yan.winterwheat
-#' dat <-t(round(acast(dat1, env~gen, value.var='yield'),2))
+#' dat <- yan.winterwheat
 #' # generates missing data
-#' dat[1,2]<-NA
-#' dat[3,4]<-NA
-#' dat[2,7]<-NA
-#' imputation(dat, rep=FALSE, type="EM-SVD")
-#' imputation(dat, rep=FALSE, type="EM-AMMI")
-#' imputation(dat, rep=FALSE, type="Gabriel")
-#' imputation(dat, rep=FALSE, type="WGabriel")
-#' imputation(dat, rep=FALSE, type="EM-PCA")
+#' dat[1,3]<-NA
+#' dat[3,3]<-NA
+#' dat[2,3]<-NA
+#' imputation(dat, genotype="gen",environment="env", response="yield", type="EM-SVD")
+#' imputation(dat, genotype="gen",environment="env", response="yield", type="EM-AMMI")
+#' imputation(dat, genotype="gen",environment="env", response="yield", type="Gabriel")
+#' imputation(dat, genotype="gen",environment="env", response="yield", type="WGabriel")
+#' imputation(dat, genotype="gen",environment="env", response="yield", type="EM-PCA")
+#'
+#'# Data with replication
+#'  data(plrv)
+#'  dat2<-plrv
+#'  dat2[1,3]<-NA
+#' dat2[3,3]<-NA
+#' dat2[2,3]<-NA
+#' imputation(dat2, genotype="gen",environment="env", response="yield", rep= "rep", type="EM-SVD")
+#' imputation(dat2, genotype="gen",environment="env", response="yield", rep= "rep", type="EM-AMMI")
+#' imputation(dat2, genotype="gen",environment="env", response="yield", rep= "rep", type="Gabriel")
+#' imputation(dat2, genotype="gen",environment="env", response="yield", rep= "rep", type="WGabriel")
+#' imputation(dat2, genotype="gen",environment="env", response="yield", rep= "rep", type="EM-PCA")
 #'
 #' @importFrom stats var
-#' @importFrom GGEBiplots stattable
 #' @importFrom bcv impute.svd
 #' @importFrom missMDA imputePCA
+#' @importFrom dplyr group_by summarise rename
 #'
-imputation <- function(Data,rep=FALSE,type="EM-AMMI") {
+imputation <- function(Data, genotype="gen",environment="env", response="yield", rep=NULL,type="EM-AMMI",
+                       PC.nb=1, initial.values=NA, precision=0.01, max.iter=1000, change.factor=1, simplified.model=FALSE,
+                       k = min(nrow(Data), ncol(Data)), tol = max(nrow(Data), ncol(Data)) * 1e-10, maxiter = 100,
+                       Winf=0.8,Wsup=1,
+                       ncp = 2, scale = TRUE, method = c("Regularized","EM"),
+                       row.w = NULL, coeff.ridge = 1, threshold = 1e-06, seed = NULL, nb.init = 1) {
 
   if (missing(Data)) stop("Need to provide Data data frame")
   if (!any(is.na(Data))) stop("There are not missing data in input data frame")
     stopifnot(
     class(Data) %in%  c("matrix", "data.frame"),
     type %in% c("EM-AMMI", "EM-SVD","Gabriel","WGabriel","EM-PCA"),
-    class(rep) == "logical"
+    class(rep)%in% c("character", "NULL")
   )
 
+  if(!is.null(rep)){
+    Data <-
+      Data %>%
+      group_by({{genotype}}, {{environment}}) %>%
+      summarise(mean_resp=mean({{response}}))%>%
+      plyr::rename(gen={{genotype}}, env= {{environment}}, mean_resp) %>%
+      spread(env, mean_resp) %>%
+      as.data.frame()
 
-  if(rep==TRUE){
-    Data<-stattable(Data[,1],Data[,2],Data[,4],FUN=mean)
-    if(type=="EM-AMMI"){
-      matrix<-EM.AMMI(Data)$X
-    }
-    if(type=="EM-SVD"){
-      matrix<-impute.svd(Data)$x
-    }
-    if(type=="Gabriel"){
-      matrix<-Gabriel.Calinski(Data)$GabrielImput
-    }
-    if(type=="WGabriel"){
-      matrix<-WGabriel(Data,0.8,1)$GabrielWImput
-    }
-    if(type=="EM-PCA"){
-      matrix<-imputePCA(Data)$completeObs
-    }
+  } else{
+    Data <-
+      Data %>%
+      spread({{environment}}, {{response}})%>%
+      as.data.frame()
   }
 
-  if(rep==FALSE){
-    if(type=="EM-AMMI"){
-        matrix<-EM.AMMI(Data)$X
-    }
-    if(type=="EM-SVD"){
-      matrix<-impute.svd(Data)$x
-    }
-    if(type=="Gabriel"){
-      matrix<-Gabriel.Calinski(Data)$GabrielImput
-    }
-    if(type=="WGabriel"){
-      matrix<-WGabriel(Data,0.8,1)$GabrielWImput
-    }
-    if(type=="EM-PCA"){
-      matrix<-imputePCA(Data)$completeObs
-    }
+  rownames(Data) <- Data[,{{genotype}}]
+  Data[,{{genotype}}] <- NULL
 
-  }
+
+  if(type=="EM-AMMI"){
+     matrix<-EM.AMMI(Data, PC.nb=PC.nb, initial.values=initial.values, precision=precision,
+                     max.iter=max.iter, change.factor=change.factor, simplified.model=simplified.model)$X
+   }
+   if(type=="EM-SVD"){
+     matrix<-impute.svd(Data, k = k, tol = tol, maxiter = maxiter)$x
+   }
+   if(type=="Gabriel"){
+     matrix<-Gabriel.Calinski(Data)$GabrielImput
+   }
+   if(type=="WGabriel"){
+     matrix<-WGabriel(Data,Winf,Wsup)$GabrielWImput
+   }
+   if(type=="EM-PCA"){
+     matrix<-imputePCA(Data, ncp = ncp, scale = scale, method = method,
+                       row.w = row.w, coeff.ridge = coeff.ridge, threshold = threshold, seed = seed, nb.init = nb.init,
+                       maxiter = maxiter)$completeObs
+   }
 
 return(matrix)
 }
